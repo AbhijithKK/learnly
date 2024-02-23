47cb02ab140f70f81b24efb52e100ba71f5c8c9f
white <abhijithkkpurameri@gmail.com>
2024-02-23 19:20:45 +0530

    home page compnent completed

--- Client/src/Components/HomePage/Home.tsx
+++ Client/src/Components/HomePage/Home.tsx
@@ -1,55 +1,89 @@
-
-import Box from '../Box/Box'
-import Button from '../Button/Button'
-import Navbar from '../Navbar/Navbar'
-import './Home.css'
+import Box from "../Box/Box";
+import Button from "../Button/Button";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+import "./Home.css";
 const Home = () => {
   return (
     <>
-
-   
       <div className="home-main">
-      
-      <Navbar/>
-       
-    <div className="home-section1">
-      <div className="home-txt-container">
-        <div className="headding">
-       Online<br/>Learning
-        </div>
-        <div className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             Vero laborum odit, suscipit tempore quam corporis 
-             veniam amet sit saepe minima ratione voluptatum optio 
-             officia in, aliquid ab sed libero deleniti?
-        </div>
-        <div style={{
-            marginTop:"30px"
-        }}>
-<Button bColor='blue' height='50px' name='REGISTER' width='150px' />
-        </div>
-      </div>
-    </div>
-    {/* section 2 scrollbar start */}
-      <div className="home-boxes">
-       <div className="home-box-title">
-       Top Courses
-       </div>
-       <div className="home-box">
+        <Navbar />
 
-        <Box title='wwww' />
-        <Box title='wwww' />
-        <Box title='wwww' />
-        <Box title='wwww' />
-        <Box title='wwww' />
-       </div>
-       <div className="home-box-more-btn">
-        View Courses &gt;
-       </div>
-      </div>
+        <div className="home-section1">
+          <div className="home-txt-container">
+            <div className="headding">
+              Online
+              <br />
+              Learning
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              laborum odit, suscipit tempore quam corporis veniam amet sit saepe
+              minima ratione voluptatum optio officia in, aliquid ab sed libero
+              deleniti?
+            </div>
+            <div
+              style={{
+                marginTop: "30px",
+              }}
+            >
+              <Button
+                bColor="blue"
+                height="50px"
+                name="REGISTER"
+                width="150px"
+              />
+            </div>
+          </div>
+        </div>
+        {/* section 2 scrollbar start */}
+        <div className="home-boxes">
+          <div className="home-box-title">Top Courses</div>
+          <div className="home-box">
+            <Box title="wwww" />
+            <Box title="wgj" />
+            <Box title="wwww" />
+            <Box title="wwww" />
+            <Box title="wwww" />
+          </div>
+          <div className="home-box-more-btn">View Courses &gt;</div>
+        </div>
+
+        {/* section 3 */}
+
+        <div className="home-section3">
+          <div className="home-txt-container">
+            <div className="headding">
+              Online
+              <br />
+              Learning
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              laborum odit, suscipit tempore quam corporis veniam amet sit saepe
+              minima ratione voluptatum optio officia in, aliquid ab sed libero
+              deleniti?
+            </div>
+            <div
+              style={{
+                marginTop: "30px",
+              }}
+            >
+              <Button
+                bColor="blue"
+                height="50px"
+                name="REGISTER"
+                width="150px"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="home-footer-mob">
+          <Footer />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
