451ce16282217bda69114e26371c3ab1bebe1132
white <abhijithkkpurameri@gmail.com>
2024-03-04 23:24:36 +0530

    navbar cart icon added

--- Client/src/Components/Navbar/Navbar.tsx
+++ Client/src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import logo from "../../assets/learnly-high-resolution-color-logo 1.png";
+import cart from '../../assets/icons8-cart-60.png'
 import "./Navbar.css";
 const Navbar = () => {
   const [profileDrop, setProfileDrop] = useState<boolean>(false);
@@ -16,27 +17,40 @@ const Navbar = () => {
             <li>Challengs</li>
           </ul>
         </div>
+
+
+
+<div className="nav-right-side">
+
+<div className="navbar-cart-img">
+        <img src={cart} alt="" className="navbar-cart-img-img"  />
+<div className="navbar-cart-item-count">
+  5
+</div>
+        </div>
+
         <div className="nav-profile">
           <div onClick={() => setProfileDrop(!profileDrop)}>
-            <img src={logo} alt="" />
+            <img src={logo} alt=""  />
             <span
               style={{
                 cursor: "pointer",
               }}
-            >
+              >
               &#x25BC;
             </span>
           </div>
           <div
             className={
               profileDrop
-                ? " nav-dropdown"
-                : "nav-dropdown nav-dropdown-toggle "
+              ? " nav-dropdown"
+              : "nav-dropdown nav-dropdown-toggle "
             }
-          >
+            >
             Logout
           </div>
         </div>
+            </div>        
       </div>
       <div className="mob-nav-main">
         <div>Home</div>|<div>Courses</div>|<div>Challengs</div>
