7e925c8d4d2d5fddb8950773c5fc4c43f48dadaa
white <abhijithkkpurameri@gmail.com>
2024-03-05 15:05:24 +0530

    payment design almost done

--- Client/src/Components/PaymentPage/Payment.tsx
+++ Client/src/Components/PaymentPage/Payment.tsx
@@ -1,3 +1,5 @@
+import Button from "../Button/Button";
+import InputBox from "../InputBox/InputBox";
 import PaymentCbox from "../PaymentCbox/PaymentCob";
 import "./Payment.css";
 
@@ -10,6 +12,8 @@ const Payment = () => {
 
         <div className="payment-section1">
           <div className="payment-address-inputpoxes">
+            <div className="payment-inputbox-only">
+              <div className="payment-address">Fill the address</div>
             <div>
 
             <select name="" id="">
@@ -28,18 +32,54 @@ const Payment = () => {
                 <option value="UAE">UP</option>
             </select>
             </div>
+            </div>
           </div>
           <div className="payment-order-details">
             <PaymentCbox hedding="web devolpoment" lesson="15 Lessons" payment="1000" discription="" image=""/>
-            {/* <Cbox hedding="web devolpoment" lesson="15 Lessons" payment="1000" discription="" image=""/> */}
+            <PaymentCbox hedding="web devolpoment" lesson="15 Lessons" payment="1000" discription="" image=""/>
+            <PaymentCbox hedding="web devolpoment" lesson="15 Lessons" payment="1000" discription="" image=""/>
           </div>
         </div>
 
         <div className="payment-section2">
-          <div className="payment-order-summery">
-            fffffffffffffffffffffff
+          <div className="payment-coopen-box">
+            
+            <InputBox
+            height="3rem"
+            width="80%"
+            name="coopenCode"
+            placeHolder="Enter Coopen Code"
+            type="text"
+            value={''}
+
+            />
+            <Button bColor="blue"
+             name="Apply" 
+             height="3rem"
+             width="20%"
+
+             />
           </div>
-          <div className="payment-coopen-box"></div>
+          <div className="payment-order-summery">
+            <div className="payment-order-summery-title">
+                Summary
+            </div>
+            <div className="payment-order-summery-items">
+                <p>Original Price:</p>
+                <p>1000</p>
+            </div>
+            <div className="payment-order-summery-items">
+                <p>Discounts:</p>
+                <p>0.00</p>
+            </div>
+            <hr />
+
+            <div className="payment-order-summery-items">
+                <p className="total">Total:</p>
+                <p className="price">1000</p>
+            </div>
+            
+          </div>
           <div className="payment-complete-checkout-btn"></div>
             </div>
         </div>
